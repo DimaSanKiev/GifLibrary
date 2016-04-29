68c610010014d90839baae8c1a8f656a15597488
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-04-30 01:48:21 +0300

    add new request mapping to GifController

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,7 +1,11 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -9,5 +13,12 @@ public class GifController {
     @RequestMapping(value = "/")
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("evolution", LocalDate.of(2016, 4, 30), "Dmytro Burdyga", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
