0dfa0afa8a31c192b6b152c7947203d4be91bf10
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-05-02 01:23:50 +0300

    update gif order

--- src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
+++ src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
@@ -11,17 +11,17 @@ import java.util.List;
 @Component
 public class GifRepository {
     private static final List<Gif> ALL_GIFS = Arrays.asList(
+        new Gif("evolution", 1, LocalDate.of(2016, 4, 30), "Dmytro Burdyga", false),
+        new Gif("flights", 3, LocalDate.of(2016, 5, 1), "Dmytro Burdyga", true),
+        new Gif("lambo", 3, LocalDate.of(2016, 5, 2), "Dmytro Burdyga", true),
+        new Gif("porsche", 3, LocalDate.of(2016, 5, 2), "Dmytro Burdyga", true),
+        new Gif("newton", 1, LocalDate.of(2016, 5, 2), "Dmytro Burdyga", true),
         new Gif("android-explosion", 1, LocalDate.of(2016, 4, 29), "Chris Ramacciotti", false),
         new Gif("ben-and-mike", 5, LocalDate.of(2016, 3, 15), "Ben Jakuben", true),
         new Gif("book-dominos", 1, LocalDate.of(2016, 4, 13), "Craig Dennis", false),
         new Gif("compiler-bot", 4, LocalDate.of(2016, 3, 11), "Ada Lovelace", true),
         new Gif("cowboy-coder", 4, LocalDate.of(2016, 2, 1), "Grace Hooper", false),
-        new Gif("infinite-andrew", 5, LocalDate.of(2016, 1, 9), "Marissa Mayer", true),
-        new Gif("evolution", 1, LocalDate.of(2016, 4, 30), "Dmytro Burdyga", false),
-        new Gif("flights", 3, LocalDate.of(2016, 5, 1), "Dmytro Burdyga", true),
-        new Gif("lambo", 3, LocalDate.of(2016, 5, 2), "Dmytro Burdyga", true),
-        new Gif("porsche", 3, LocalDate.of(2016, 5, 2), "Dmytro Burdyga", true),
-        new Gif("newton", 1, LocalDate.of(2016, 5, 2), "Dmytro Burdyga", true)
+        new Gif("infinite-andrew", 5, LocalDate.of(2016, 1, 9), "Marissa Mayer", true)
     );
 
     public Gif findByName(String name) {
