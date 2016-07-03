2434fa7ecd7c675774227e41a8b23ffc731a7c9a
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-03 21:23:24 +0300

    refactor project

---
+++ src/main/java/org/education/giflib/dao/CategoryDao.java
@@ -0,0 +1,12 @@
+package org.education.giflib.dao;
+
+import org.education.giflib.model.Category;
+
+import java.util.List;
+
+public interface CategoryDao {
+    List<Category> findAll();
+    Category findById(Long id);
+    void save(Category category);
+    void delete(Category category);
+}
