783490d52f8f7538ced7372ecefc213ae004830e
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-06 21:04:37 +0300

    add GifService

--- src/main/java/org/education/giflib/web/controller/GifController.java
+++ src/main/java/org/education/giflib/web/controller/GifController.java
@@ -6,11 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class GifController {
     @Autowired
     CategoryService categoryService;
+
+    // upload a new GIF
+    @RequestMapping(value = "/gifs", method = RequestMethod.POST)
+    public String addGif(@RequestParam MultipartFile file) {
+        return null;
+    }
 
     // form for uploading a new GIF
     @RequestMapping("/upload")
