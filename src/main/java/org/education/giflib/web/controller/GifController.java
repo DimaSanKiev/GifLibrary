b3c75adab3059b596e37b01d187f8c0c27c04d2b
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-06 22:43:59 +0300

    add methods to GifController to persist GIFs

--- src/main/java/org/education/giflib/web/controller/GifController.java
+++ src/main/java/org/education/giflib/web/controller/GifController.java
@@ -2,23 +2,59 @@ package org.education.giflib.web.controller;
 
 import org.education.giflib.model.Gif;
 import org.education.giflib.service.CategoryService;
+import org.education.giflib.service.GifService;
+import org.education.giflib.web.FlashMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.education.giflib.web.FlashMessage.Status.SUCCESS;
 
 @Controller
 public class GifController {
+
     @Autowired
-    CategoryService categoryService;
+    private GifService gifService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    // home page - list of all GIFs
+    @RequestMapping("/")
+    public String listGifs(Model model) {
+        List<Gif> gifs = new ArrayList<>();
+        model.addAttribute("gifs", gifs);
+        return "gif/index";
+    }
+
+    // single GIF page
+    @RequestMapping("/gifs/{gifId}")
+    public String gifDetails(@PathVariable Long gifId, Model model) {
+        Gif gif = gifService.findById(gifId);
+        model.addAttribute("gif", gif);
+        return "gif/details";
+    }
+
+    // GIF image data
+    @RequestMapping("gifs/{gifId}.gif")
+    @ResponseBody
+    public byte[] getImage(@PathVariable Long gifId) {
+        return gifService.findById(gifId).getBytes();
+    }
 
     // upload a new GIF
     @RequestMapping(value = "/gifs", method = RequestMethod.POST)
-    public String addGif(@RequestParam MultipartFile file) {
-        return null;
+    public String addGif(Gif gif, @RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
+        gifService.save(gif, file);
+        // add a flash message for success
+        redirectAttributes.addFlashAttribute("flash", new FlashMessage("GIF successfully uploaded!", SUCCESS));
+        return String.format("redirect:/gifs/%s", gif.getId());
     }
 
     // form for uploading a new GIF
